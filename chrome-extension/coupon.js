dbdef4e83e5f3e64764c9a1bfce0a38cb23ee145
alanhe <qianghe421@gmail.com>
2020-05-31 14:40:32 +0800

    add jd chrome extension

--- chrome-extension/coupon.js
+++ chrome-extension/coupon.js
@@ -1,36 +1,23 @@
-// 5,10,20,40,50
-let coupons = document.getElementsByClassName('full-coupon-list')[0].children;
+function startPlay(selectedCoupon) {
+    // 点击事件
+    const e = document.createEvent("MouseEvents");
+    e.initEvent("click", true, true);
 
-const fiveRMB = 0;
-const tenRMB = 1;
-const twentyRMB = 2;
-const fortyRMB = 3;
-const fiftyRMB = 4;
+    let couponsElements = document.getElementsByClassName('full-coupon-list')[0].children;
 
-// 选中想领取的券类型
-const selectedCoupon = [fiveRMB];
-
-// 点击事件
-const e = document.createEvent("MouseEvents");
-e.initEvent("click", true, true);
-
-// 每隔一秒去检查目标券领取状态，如果可以领取则点击
-let interval = window.setInterval(() => {
-        if (selectedCoupon.length === 0) {
-            window.clearInterval(interval);
+    if (selectedCoupon.length === 0) {
+        return;
+    }
+    selectedCoupon.forEach(idx => {
+        let btn = couponsElements[idx].getElementsByClassName('full-coupon-btn')[0];
+        let content = btn.textContent;
+        if (content === '领取') {
+            btn.dispatchEvent(e);
+            // 确认领取
+            document.getElementsByClassName('btn-wrap btn-wrap-1 J_ping')[0].dispatchEvent(e);
+            // 关闭确认框
+            document.getElementsByClassName('close fr')[0].dispatchEvent(e);
+            selectedCoupon.pop();
         }
-        selectedCoupon.forEach(idx => {
-            let btn = coupons[idx].getElementsByClassName('full-coupon-btn')[0];
-            let content = btn.textContent;
-            if (content === '领取') {
-                btn.dispatchEvent(e);
-                // 确认领取
-                document.getElementsByClassName('btn-wrap btn-wrap-1 J_ping')[0].dispatchEvent(e);
-                // 关闭确认框
-                document.getElementsByClassName('close fr')[0].dispatchEvent(e);
-                selectedCoupon.pop();
-            }
-        })
-    },
-    2000
-);
+    })
+}
---
+++ chrome-extension/options.js
@@ -0,0 +1,27 @@
+function onSubmit() {
+    let elementsByName = document.getElementsByName('coupon');
+    const savedCoupons = [];
+    elementsByName.forEach(item => {
+        if (item.checked) {
+            savedCoupons.push(+item.value);
+        }
+    })
+    chrome.storage.sync.set({
+        coupons: savedCoupons
+    });
+}
+
+function restoreOptions() {
+    chrome.storage.sync.get({coupons: []}, function ({coupons}) {
+        document.getElementsByName('coupon').forEach(
+            item => {
+                if (coupons.includes(+item.value)) {
+                    item.checked = true;
+                }
+            }
+        )
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('couponForm').addEventListener('click', onSubmit);
